b3c84de64d4f1981d37220f53c073c80fdb2f5da
Kamuish <andremiguel952@gmail.com>
2020-01-30 15:22:32 +0000

    utility module

---
+++ src/utils/__init__.py
@@ -0,0 +1,1 @@
+from create_print_file import create_print_file
---
+++ src/utils/create_print_file.py
@@ -0,0 +1,9 @@
+
+
+def create_print_file(filename, *args):
+    with open(filename + options[chosen_option], mode = 'w') as file:
+        print(*args, file = file)
+
+
+if __name__ == '__main__':
+    create_print_file('teste.txt', [''],0, *args = 'a','b')
