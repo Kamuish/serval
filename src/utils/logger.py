e1a75dc80d3832580f94a6b589720bef3accf0f1
Kamuish <andremiguel952@gmail.com>
2020-02-10 22:01:00 +0000

    fixing naming problem with the logger

--- src/utils/logger.py
+++ src/utils/logger.py
@@ -1,14 +1,16 @@
 import logging 
+import sys 
 
 def setup_logger():
-    logger = logging.getLogger('serval')
+    logger = logging.getLogger('src')
+
     logger.setLevel(logging.DEBUG)
     # create file handler which logs even debug messages
     fh = logging.FileHandler('serval.log',mode='w')
     fh.setLevel(logging.DEBUG)
     # create console handler with a higher log level
-    ch = logging.StreamHandler()
-    ch.setLevel(logging.INFO)
+    ch = logging.StreamHandler(sys.stderr)
+    ch.setLevel(logging.DEBUG)
     # create formatter and add it to the handlers
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     fh.setFormatter(formatter)
@@ -16,3 +18,4 @@ def setup_logger():
     # add the handlers to the logger
     logger.addHandler(fh)
     logger.addHandler(ch)
+
